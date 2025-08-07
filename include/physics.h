f2e9d97e99cda44ab66108490849953340bf2c1b
eric <e.salberg213@gmail.com>
2025-08-06 21:48:22 -0400

    Added collision handling with 2 balls

--- include/physics.h
+++ include/physics.h
@@ -1,5 +1,10 @@
 #pragma one
+
 #include <raylib.h>
+#include <stdbool.h>
+
+#define RESTITUTION_BALL_BALL 1
+#define RESTITUTION_BALL_WALL 1
 
 typedef struct {
   Vector2 position;
@@ -7,3 +12,7 @@ typedef struct {
   float mass;
   int radius;
 } Ball;
+
+bool hasCollided(Ball b1, Ball b2);
+void handleBallToBallCollision(Ball *b1, Ball *b2);
+void handleBallToWallCollision(Ball *b);
--- main.c
+++ main.c
@@ -1,53 +1,70 @@
 #include "physics.h"
 #include "raylib.h"
 
-int main(void) {
-  const int screenWidth = 1200;
-  const int screenHeight = 960;
-  float deltaTime = 0.0f;
-  bool hasBallBeenHit = false;
-  Vector2 mousePosition = {0};
+static const int screenWidth = 1200;
+static const int screenHeight = 960;
 
-  Ball ball = {.position = {(float)screenWidth / 2, (float)screenHeight / 2},
-               .velocity = {0.0f, 0.0f},
-               .mass = 10,
-               .radius = 50};
+static float deltaTime = 0.0f;
+static bool hasqBallBeenHit = false;
+static Vector2 mousePosition = {0};
+
+static Ball qBall = {
+    .position = {(float)screenWidth / 2, (float)screenHeight / 2},
+    .velocity = {0.0f, 0.0f},
+    .mass = 10,
+    .radius = 50};
+static Ball redBall = {
+    .position = {(float)screenWidth / 2, (float)screenHeight / 4},
+    .velocity = {0.0f, 0.0f},
+    .mass = 10,
+    .radius = 50};
+
+void update() {
+  deltaTime = GetFrameTime();
+
+  if (!hasqBallBeenHit) {
+    mousePosition = GetMousePosition();
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+      hasqBallBeenHit = true;
+      qBall.velocity.x = 3 * (qBall.position.x - mousePosition.x);
+      qBall.velocity.y = 3 * (qBall.position.y - mousePosition.y);
+    }
+  }
+
+  qBall.position.x += qBall.velocity.x * deltaTime;
+  qBall.position.y += qBall.velocity.y * deltaTime;
+  redBall.position.x += redBall.velocity.x * deltaTime;
+  redBall.position.y += redBall.velocity.y * deltaTime;
+
+  if (hasCollided(qBall, redBall)) {
+    handleBallToBallCollision(&qBall, &redBall);
+  }
+  handleBallToWallCollision(&qBall);
+  handleBallToWallCollision(&redBall);
+}
+
+void draw() {
+  BeginDrawing();
+  ClearBackground(DARKGREEN);
+  DrawText(TextFormat("Q - x:%.0f, y:%.0f", qBall.velocity.x, qBall.velocity.y),
+           10, 30, 20, WHITE);
+  DrawText(TextFormat("RED - x:%.0f, y:%.0f", redBall.velocity.x,
+                      redBall.velocity.y),
+           10, 10, 20, RED);
+
+  DrawCircleV(qBall.position, qBall.radius, WHITE);
+  DrawCircleV(redBall.position, redBall.radius, RED);
+  EndDrawing();
+}
+
+int main(void) {
 
   InitWindow(screenWidth, screenHeight, "raylib window");
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
-    deltaTime = GetFrameTime();
-
-    if (!hasBallBeenHit) {
-      mousePosition = GetMousePosition();
-      if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-        hasBallBeenHit = true;
-        ball.velocity.x = ball.position.x - mousePosition.x;
-        ball.velocity.y = ball.position.y - mousePosition.y;
-      }
-    }
-
-    ball.position.x += ball.velocity.x * deltaTime;
-    ball.position.y += ball.velocity.y * deltaTime;
-    if (ball.position.x >= (GetScreenWidth() - ball.radius) ||
-        ball.position.x <= ball.radius) {
-      ball.velocity.x *= -1.0f;
-    }
-    if (ball.position.y >= (GetScreenHeight() - ball.radius) ||
-        ball.position.y <= ball.radius) {
-      ball.velocity.y *= -1.0f;
-    }
-
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    if (!hasBallBeenHit) {
-      DrawText(TextFormat("Mouse Position - x:%.0f, y:%.0f", mousePosition.x,
-                          mousePosition.y),
-               10, 10, 20, LIGHTGRAY);
-    }
-    DrawCircleV(ball.position, ball.radius, MAROON);
-    EndDrawing();
+    update();
+    draw();
   }
 
   return 0;
